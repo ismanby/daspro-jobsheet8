b549551fafceed37d6e161bf5c41969d7e2577c3
ismanby <ismaanby@gmail.com>
2025-10-30 10:18:51 +0700

    Percobaan 1

---
+++ Star12.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Star12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        // input nilai N
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+
+        // looping * sebangak N
+        for (int i = 1; i <= N; i++) {
+            System.out.print("*");
+        }
+
+        sc.close();
+    }
+}
